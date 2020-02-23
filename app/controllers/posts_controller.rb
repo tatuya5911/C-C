7ce20c253878d60553afdfc2407acf677c8a49cf
tatuya5911 <kkb76904@gmail.com>
2020-02-23 03:27:47 +0000

    post create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,57 @@
 class PostsController < ApplicationController
+
+  before_action :set_categorys, only: [:new, :create, :edit, :update]
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    @category = @post.category
+    @user = @post.user
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    post = Post.new(post_params)
+    post.user_id = current_user.id
+    if post.save
+      redirect_to post_path(post.id), notice:"投稿しました"
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+    @user = @post.user
+    @category = @post.category
+  end
+
+  def update
+    post = Post.find(params[:id])
+    post.update(post_params)
+    redirect_to post_path
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:user_id, :category_id, :post_image, :post_title, :body, :aroma, :acidity, :rich, :bitterness, :rate)
+  end
+
+  def set_categorys
+    @categorys = Category.all
+  end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
+
+  belongs_to :user
+  belongs_to :category
+
+  attachment :post_image
 end
