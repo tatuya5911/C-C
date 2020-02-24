b9837714aac2c1590ee249f6204508f3d98b9334
tatuya5911 <kkb76904@gmail.com>
2020-02-24 06:19:53 +0000

    create show edit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:user_name, :email, :profile_image, :encrypted_password)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'homes#top'
   resources :categorys, only: [:index, :create, :edit, :update, :destroy]
-  
+  resources :users, only: [:show, :edit, :update]
+
 end
