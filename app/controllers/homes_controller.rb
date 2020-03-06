187e6853fb30da653dcce1f09a7cc75233c64509
tatuya5911 <kkb76904@gmail.com>
2020-03-06 06:42:11 +0000

    update layout

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,6 +3,7 @@ class HomesController < ApplicationController
   def top
     @user = current_user
     @posts = Post.all
+    @categorys = Category.all
     @fresh_posts = Post.all.order(created_at: :desc).limit(10)
     @all_ranks = Post.find(Like.group(:post_id).order('count(post_id) desc').limit(10).pluck(:post_id))
     @histories = @user.browsing_histories.all.limit(10)
