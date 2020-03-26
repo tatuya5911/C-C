01fcab57a473e1b5fd3940166185f00e5b5597b6
tatuya5911 <kkb76904@gmail.com>
2020-03-26 14:31:42 +0000

    UserController update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,8 +25,11 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to user_path(@user.id)
+    if @user.update(user_params)
+      flash[:success] = "更新しました。"
+      redirect_to user_path(@user.id)
+    else
+      flash[:alert] = "更新に失敗しました。再度お願いいたします。"
   end
 
   def following
@@ -40,7 +43,6 @@ class UsersController < ApplicationController
     @users = @user.followers.page(params[:page]).per(30)
     render 'follower'
   end
-
 
   private
 
